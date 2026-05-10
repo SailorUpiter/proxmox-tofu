eff7bac0d72319dc7e90c1878a97655c02891663
SailorUpiter <medik19999@mail.ru>
2026-05-10 11:49:35 +0300

    add ssh-keys gen module

--- modules/ssh/variables.tf
+++ modules/ssh/variables.tf
@@ -3,8 +3,10 @@
 variable "private_key_file" {
   description = "SSH privte key file"
   type        = string
+  default     = "./"
 }
 variable "public_key_file" {
   description = "SSH public key file"
   type        = string
+  default     = "./"
 }
